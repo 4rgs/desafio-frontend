6e2e35cdd66396e5df328d4be6b29b452421427a
spids <spids@msi.spids.cl>
2022-02-15 19:21:18 -0300

    remove unused imports

--- src/components/buscador/buscador.test.js
+++ src/components/buscador/buscador.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Buscador from './/buscador'
 import { mount , shallow} from 'enzyme'
-import testProducts from '../..//../utils/testProducts'
 
 const busqueda = [
     {
